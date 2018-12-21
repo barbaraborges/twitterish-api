217e03ae3c663e29141540234937c37bcb8f9288
barbaraborges <basborges@gmail.com>
2018-12-21 10:59:35 -0200

    Edited user migration

--- db/migrate/20181221120830_create_users.rb
+++ db/migrate/20181221120830_create_users.rb
@@ -2,8 +2,11 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email
+      t.string :nickname
       t.string :password_digest
+      t.string :email
+      t.string :pic
+      t.string :about
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_20_125213) do
+ActiveRecord::Schema.define(version: 2018_12_21_120830) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
+    t.string "nickname"
     t.string "password_digest"
+    t.string "email"
+    t.string "pic"
+    t.string "about"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
