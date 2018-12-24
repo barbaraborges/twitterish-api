cfb50d9713193960d820870954937bd00b35f131
barbaraborges <basborges@gmail.com>
2018-12-24 19:00:59 -0200

    Added Likes

--- db/migrate/20181221132722_create_tweets.rb
+++ db/migrate/20181221132722_create_tweets.rb
@@ -1,9 +1,8 @@
 class CreateTweets < ActiveRecord::Migration[5.2]
   def change
     create_table :tweets do |t|
-      t.belongs_to :author, index: true 
       t.string :content
-      t.boolean :likes
+      t.references :user
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_21_120830) do
+ActiveRecord::Schema.define(version: 2018_12_22_170809) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tweet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tweet_id"], name: "index_likes_on_tweet_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "nickname"
+    t.string "username"
     t.string "password_digest"
     t.string "email"
     t.string "pic"
